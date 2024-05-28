b4a655f32e092ac10dc462a6e9beac100d638604
Babar Mohammad Iqbal <138586658+BabarBurhan@users.noreply.github.com>
2024-05-28 12:39:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+// let message = "Hello World"
+// console.log(message);
+let personName = '';
+personName = prompt('What is your name?') || '';
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, would you like to learn some python today?`);
+}
+else {
+    alert('you have to fill your name !');
+}
---
+++ app.ts
@@ -0,0 +1,15 @@
+// let message = "Hello World"
+
+// console.log(message);
+
+let personName :string = '';
+
+personName = prompt('What is your name?') || '';
+
+if(personName !== null && personName !== ''){
+    alert(`Hello ${personName}, would you like to learn some python today?`)
+}
+else{
+    alert('you have to fill your name !')
+}
+
